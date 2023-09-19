ab5a44e6e3ff7bb0950ca9eff6277a169a281513
ron4ldblue <ronaldblue54@gmail.com>
2023-09-18 22:01:20 -0300

    operadores parte final

---
+++ java-anatomia-classes/src/edu/ronaldo/segundasemana/Operadores.java
@@ -0,0 +1,26 @@
+package edu.ronaldo.segundasemana;
+public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao ="?";
+        
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
---
+++ java-anatomia-classes/src/edu/ronaldo/segundasemana/OperadoresUnarios.java
@@ -0,0 +1,16 @@
+package edu.ronaldo.segundasemana;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+
+        int numero = 5; 
+
+         numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+}
