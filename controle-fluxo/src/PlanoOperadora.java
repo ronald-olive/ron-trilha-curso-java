d88b69a41dc1976c5eaf9b45214a251eed843ff6
ron4ldblue <ronaldblue54@gmail.com>
2023-09-26 13:05:14 -0300

    controle de fluxo

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,16 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if (plano == "B") {
+            System.out.println("100 minutos de ligação");
+        }else if (plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e instagram grátis");
+        }else if (plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e instagram grátis");
+            System.out.println("5Gb Youtube");
+        }
+    }
+}
